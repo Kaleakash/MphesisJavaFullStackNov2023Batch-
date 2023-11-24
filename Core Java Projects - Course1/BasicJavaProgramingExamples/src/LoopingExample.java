4d3b8e8d66de4aa4f82b0b9549295f2842256303
akash <akash300383@gmail.com>
2023-11-24 14:55:49 +0530

    done

---
+++ Core Java Projects - Course1/BasicJavaProgramingExamples/src/LoopingExample.java
@@ -0,0 +1,28 @@
+
+public class LoopingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		System.out.println("While loop");
+//		int i=1,n=10;
+//		while(i<=n) {	
+//			//System.out.println("Hello");
+//			System.out.println("i "+i);
+//			i++;
+//		}
+		
+//		System.out.println("Do While loop");
+//		int i=1,n=10;
+//		do {	
+//			//System.out.println("Hello");
+//			System.out.println("i "+i);
+//			i++;
+//		}while(i<=n);
+		
+		System.out.println("for loop");
+		for(int i=1,n=10;i<=n;i++) {
+			System.out.println("i "+i);
+		}
+	}
+
+}
