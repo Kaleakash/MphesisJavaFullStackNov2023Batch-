65ddc03bc4408013037186f5255414dac70b34a1
akash <akash300383@gmail.com>
2023-11-24 17:36:28 +0530

    done

--- Core Java Projects - Course1/OOPsConceptExamples/src/com/Car.java
+++ Core Java Projects - Course1/OOPsConceptExamples/src/com/Car.java
@@ -20,9 +20,19 @@ public class Car {
 		System.out.println("Car stop");
 	}
 	
-	void displayCarInfo() {
+//	void setCarInfo(int wheel1, String colour1,float price1) {
+//		wheel=wheel1;
+//		colour=colour1;
+//		price=price1;
+//	}
+	void setCarInfo(int wheel, String colour,float price) {
+		this.wheel=wheel;	// this.wheel is instance and wheel is local 
+		this.colour=colour;
+		this.price=price;
+	}
+	void displayCarInfo(String name) {
 		String msg="Car Info";		// local variable 
-		System.out.println(msg);
+		System.out.println(msg+" : "+name);
 		System.out.println("Wheel "+wheel);
 		System.out.println("Colour "+colour);
 		System.out.println("Price "+price);
--- Core Java Projects - Course1/OOPsConceptExamples/src/com/CarTest.java
+++ Core Java Projects - Course1/OOPsConceptExamples/src/com/CarTest.java
@@ -18,11 +18,21 @@ public class CarTest {
 //	Car bmw = new Car();
 		
 	Car innova = new Car();
-	innova.displayCarInfo();
-	innova.wheel=4;
-	innova.colour="White";
-	innova.price=3200000;
-	innova.displayCarInfo();
+	//innova.displayCarInfo();
+//	innova.wheel=4;
+//	innova.colour="White";
+//	innova.price=3200000;
+	innova.setCarInfo(4, "White", 3200000);
+	innova.displayCarInfo("Innova");
+	
+	Car ertiga = new Car();
+	//ertiga.displayCarInfo();
+//	ertiga.wheel=4;
+//	ertiga.colour="Gray";
+//	ertiga.price=1300000;
+	ertiga.setCarInfo(4, "Gray", 1300000);
+	ertiga.displayCarInfo("Ertiga");
+	
 	}
 
 }
