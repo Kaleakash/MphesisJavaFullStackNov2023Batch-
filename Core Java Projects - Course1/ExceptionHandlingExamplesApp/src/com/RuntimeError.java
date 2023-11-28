bf5fa843c1562219c67a21d070c55f7e1a6b355b
akash <akash300383@gmail.com>
2023-11-28 17:17:18 +0530

    done

--- Core Java Projects - Course1/ExceptionHandlingExamplesApp/src/com/RuntimeError.java
+++ Core Java Projects - Course1/ExceptionHandlingExamplesApp/src/com/RuntimeError.java
@@ -5,10 +5,13 @@ public class RuntimeError {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		int a=10;
-		int b=0;
+		int b=1;
+		int abc[]= {100,200,300,400};
 			try {
-			int res = a/b;
+				int res = a/b;
 			System.out.println("Result is "+res);
+				int res1 = 100/abc[5];
+			System.out.println("Result "+res1);
 			}catch(Exception e) {
 				//System.out.println("I Take Care!");
 				//System.out.println(e.getMessage());  // short message of exception 
---
+++ Core Java Projects - Course1/ExceptionHandlingExamplesApp/src/com/TryWithMultipleCatchBlock.java
@@ -0,0 +1,29 @@
+package com;
+
+public class TryWithMultipleCatchBlock {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		int abc[]= {10,20};
+		String s1 = "10a";
+		try {
+			int res1 = 100/1;
+			System.out.println("Result is "+res1);
+			int res2 = abc[0];
+			System.out.println("Result is "+res2);
+			System.out.println(s1+10);
+				System.out.println(Integer.parseInt(s1)+10);
+			System.out.println("No Exception");
+		}
+		catch (ArithmeticException e) {
+			System.out.println("Divided by Zero "+e.toString());
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Array index "+e.toString());
+		}catch(Exception e) {
+			System.out.println("generic "+e.toString());
+		}
+		
+		System.out.println("Normal Statement");
+	}
+
+}
