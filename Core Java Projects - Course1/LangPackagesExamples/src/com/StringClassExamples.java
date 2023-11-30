7b452214f9d6f0363c88924c3a6ef343346f0b0d
akash <akash300383@gmail.com>
2023-11-30 14:24:01 +0530

    done

---
+++ Core Java Projects - Course1/LangPackagesExamples/src/com/StringClassExamples.java
@@ -0,0 +1,32 @@
+package com;
+
+public class StringClassExamples {
+
+	public static void main(String[] args) {
+//	String str1 = "Welcome to Java Training";
+//	String str2 = new String("Welcome to Java Training");
+//	System.out.println(str1);
+//	System.out.println(str2);
+//	System.out.println(str1.length());
+//	System.out.println(str1.toUpperCase());
+//	System.out.println(str1.toLowerCase());
+//	System.out.println(str1.charAt(0));
+//	System.out.println(str1.indexOf('o'));
+//	System.out.println(str1.substring(5));
+		String name1 = "Ravi";
+		String name2 = "Ravi";		// only one memory string pooling 
+	String name3 = new String("Ravi");
+	String name4 = new String("Ravi");
+//		if(name1==name3) {
+//			System.out.println("Equal");
+//		}else {
+//			System.out.println("Not Equal");
+//		}
+	if(name1.equals(name4)) {
+		System.out.println("Equal");
+	}else {
+		System.out.println("Not Equal");
+	}
+	}
+
+}
