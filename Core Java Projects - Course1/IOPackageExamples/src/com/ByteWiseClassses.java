6a77343bfe7b3352a1126923b364ccbc09b54e78
akash <akash300383@gmail.com>
2023-11-30 16:54:14 +0530

    done

--- Core Java Projects - Course1/IOPackageExamples/src/com/ByteWiseClassses.java
+++ Core Java Projects - Course1/IOPackageExamples/src/com/ByteWiseClassses.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
@@ -14,17 +15,42 @@ public class ByteWiseClassses {
 //		String info = dis.readLine();
 //		ps.println(info);
 		
-		// source :keyword , destination : file 
-				DataInputStream dis = new DataInputStream(System.in);
-				FileOutputStream fos = new FileOutputStream("abc.txt");
-				PrintStream ps = System.out;
-				ps.println("Enter the data");
-				int ch;
-				while((ch = dis.read())!= '@') {
-					fos.write(ch);
-				}
-				fos.close();
-				System.out.println("Data stored in file");
+//		// source :keyword , destination : file 	override the value 
+//				DataInputStream dis = new DataInputStream(System.in);
+//				FileOutputStream fos = new FileOutputStream("abc.txt");
+//				PrintStream ps = System.out;
+//				ps.println("Enter the data");
+//				int ch;
+//				while((ch = dis.read())!= '@') {
+//					fos.write(ch);
+//				}
+//				fos.close();
+//				System.out.println("Data stored in file");
+		
+		
+//		// source :keyword , destination : file append the value 
+//		DataInputStream dis = new DataInputStream(System.in);
+//		FileOutputStream fos = new FileOutputStream("xyz.txt",true);
+//		PrintStream ps = System.out;
+//		ps.println("Enter the data");
+//		int ch;
+//		while((ch = dis.read())!= '@') {
+//			fos.write(ch);		// type casting 
+//			System.out.print(ch+" "+(char)ch);
+//		}
+//		fos.close();
+//		System.out.println("Data stored in file");
+		
+		// source : file , destination : file 
+		FileInputStream fis = new FileInputStream("xyz.txt");
+		FileOutputStream fos = new FileOutputStream("D:\\demo.txt");
+		int ch;
+		while((ch=fis.read()) != -1) {		// -1 = EOF
+			fos.write(ch);
+		}
+		fis.close();
+		fos.close();
+		System.out.println("file copied....");
 	}
 
 }
