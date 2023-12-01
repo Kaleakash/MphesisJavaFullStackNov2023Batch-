3d54c58e854aeebb29ba6843ee0c455713b486f9
akash <akash300383@gmail.com>
2023-12-01 16:36:03 +0530

    done

---
+++ Core Java Projects - Course1/CollectionFrameworkApp/src/com/HashSetDemo.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+	HashSet hs = new HashSet();
+	System.out.println("Size is "+hs.size());
+	System.out.println("Empty "+hs.isEmpty());
+	int a=10;
+	//Integer b = new Integer(a);
+	Integer b=a;		// converting primitive to object. 
+	hs.add(b);
+	hs.add(a);	//auto-boxing : converting primitive to object. 
+	hs.add(20);
+	hs.add(20.20);
+	hs.add(true);
+	hs.add("Ravi");
+	System.out.println("Size is "+hs.size());
+	
+	}
+
+}
