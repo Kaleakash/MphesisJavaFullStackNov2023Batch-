499bab7f557546ba52cf09abdf29c656caad47b2
akash <akash300383@gmail.com>
2023-12-04 14:59:21 +0530

    done

---
+++ Core Java Projects - Course1/CollectionFrameworkApp/src/com/MapDemo.java
@@ -0,0 +1,42 @@
+package com;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+	// HashMap 
+	//Map mm = new HashMap();		// Unorder 
+	// LinkedHashMap  
+	//Map mm = new LinkedHashMap();	// maintain the order
+	// TreeMap 
+	//Map mm = new TreeMap();			// TreeMap asc order as key 
+	// Hashtable 
+		Map mm = new Hashtable();		// all methods are synchronized 
+	mm.put(2, "Ravi");
+	mm.put(1, "Rajesh");
+	mm.put(5, "Lokesh");
+	mm.put(3, "Ramesh");
+		mm.put("A", "B");
+	System.out.println(mm);
+		mm.put(1, "Ajay");
+	mm.put(7, "Ramesh");
+	System.out.println(mm);
+	
+//	if(mm.containsKey(10)) {
+//		System.out.println("Key present");
+//	}else {
+//		System.out.println("key not present");
+//	}
+//	System.out.println("VAlue "+mm.containsValue("Ravi"));
+//	System.out.println("VAlue "+mm.containsValue("Raju"));
+//	System.out.println("Get VAlue "+mm.get(2));
+//	System.out.println("Get VAlue "+mm.get(20));
+	
+	}
+
+}
