4f42c32a64d4864a1758c5c902bb323f12894254
akash <akash300383@gmail.com>
2023-12-04 15:55:14 +0530

    done

--- Core Java Projects - Course1/CollectionFrameworkApp/src/com/RetrieveValueFromCollectionsOneByOne.java
+++ Core Java Projects - Course1/CollectionFrameworkApp/src/com/RetrieveValueFromCollectionsOneByOne.java
@@ -2,10 +2,12 @@ package com;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
@@ -28,31 +30,54 @@ public class RetrieveValueFromCollectionsOneByOne {
 //	 }
 	// List API , LinkedList and ArrayList 
 	
-	List ll = new ArrayList();
-	ll.add("Ravi");ll.add("Ramesh");ll.add("Ajay");ll.add("Vijay");
-	System.out.println(ll);
-	// for each loop or enhanced loop or iterator 
-	ListIterator li = ll.listIterator();
-	System.out.println("forward direction");
+//	List ll = new ArrayList();
+//	ll.add("Ravi");ll.add("Ramesh");ll.add("Ajay");ll.add("Vijay");
+//	System.out.println(ll);
+//	// for each loop or enhanced loop or iterator 
+//	ListIterator li = ll.listIterator();
+//	System.out.println("forward direction");
+//	while(li.hasNext()) {
+//		Object obj = li.next();
+//		System.out.println(obj);
+//	}
+//	System.out.println("backward direction");
+//	while(li.hasPrevious()) {
+//		Object obj = li.previous();
+//		System.out.println(obj);
+//	}
+//	Vector vv = new Vector();
+//	vv.add(3); vv.add(7);vv.add(10);
+//	System.out.println("Retrieve value from Vector");
+//	Enumeration ee = vv.elements();
+//	while(ee.hasMoreElements()) {
+//		Object obj = ee.nextElement();
+//		System.out.println(obj);
+//	}
+		
+	Map mm = new HashMap();
+	mm.put(1, "Ravi");
+	mm.put(2, "Ramesh");
+	mm.put(3,"Lokesh");
+	// map we can't use for each loop, iterator, listiterator as well as enumeration 
+	Set ss = mm.keySet();		//converted all key from map to set. 
+	Iterator li = ss.iterator();
 	while(li.hasNext()) {
-		Object obj = li.next();
-		System.out.println(obj);
+		Object key = li.next();
+		System.out.println("Key "+key+" Value "+mm.get(key));
 	}
-	System.out.println("backward direction");
-	while(li.hasPrevious()) {
-		Object obj = li.previous();
-		System.out.println(obj);
-	}
-	Vector vv = new Vector();
-	vv.add(3); vv.add(7);vv.add(10);
-	System.out.println("Retrieve value from Vector");
-	Enumeration ee = vv.elements();
-	while(ee.hasMoreElements()) {
-		Object obj = ee.nextElement();
-		System.out.println(obj);
+	Set ss1 = mm.entrySet();		// converted map to set. 
+	Iterator li1 = ss1.iterator();
+	while(li1.hasNext()) {
+		//Object obj = li1.next();
+		//System.out.println(obj);
+		Map.Entry me = (Map.Entry)li1.next();
+		System.out.println("key "+me.getKey()+" value "+me.getValue());
 	}
 	}
 
 }
+
+
+
 
 
