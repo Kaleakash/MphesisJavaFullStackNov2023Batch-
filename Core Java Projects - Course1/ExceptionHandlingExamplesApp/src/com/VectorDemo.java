de12d4e58005ffdce2aab09e86d425f54b13a9de
akash <akash300383@gmail.com>
2023-12-04 14:13:45 +0530

    done

---
+++ Core Java Projects - Course1/ExceptionHandlingExamplesApp/src/com/VectorDemo.java
@@ -0,0 +1,16 @@
+package com;
+
+import java.util.Vector;
+
+public class VectorDemo {
+
+	public static void main(String[] args) {
+		Vector vv = new Vector();
+		vv.add(10);
+		vv.add(20);
+		vv.add(30);
+		System.out.println(vv);
+
+	}
+
+}
