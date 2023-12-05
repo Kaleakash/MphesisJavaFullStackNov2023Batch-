3187217572bfaa60301514b2551ed4897179f6de
akash <akash300383@gmail.com>
2023-12-05 17:16:00 +0530

    done

---
+++ Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/CollectionsDemo.java
@@ -0,0 +1,37 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsDemo {
+	public static void main(String[] args) {
+	List<String> names = new ArrayList<>();
+	names.add("Ravi");
+	names.add("Ajay");
+	names.add("Vikash");
+	names.add("Dinesh");
+	System.out.println("Befor sort");
+	for(String name:names) {
+		System.out.print(name+" ");
+	}
+	System.out.println();
+	System.out.println("After sort - Asc ");
+		Collections.sort(names);
+	for(String name: names) {
+		System.out.print(name+" ");
+	}
+		Collections.reverse(names);
+	System.out.println();
+	System.out.println("After sort - Desc");
+	for(String name: names) {
+		System.out.print(name+" ");
+	}
+	System.out.println();
+	if(Collections.binarySearch(names, "Rajesh")<0) {
+		System.out.println("Name not present");
+	}else {
+		System.out.println("Name present");
+	}
+	}
+}
