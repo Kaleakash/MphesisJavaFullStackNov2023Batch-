96b7266b94c1de221633a90324b0c989a6e0f77e
akash <akash300383@gmail.com>
2023-12-05 15:57:03 +0530

    done

--- Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SortingExamples.java
+++ Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SortingExamples.java
@@ -74,6 +74,53 @@ class Sorting{
 		}
 	}
 	
+	
+	public static void merge(int num[], int start, int mid, int end) {
+		int leftSize = mid-start +1;
+		int rightSize = end - mid;
+		int leftArray[]=new int[leftSize];
+		int rightArray[]= new int[rightSize];
+		
+		for(int i=0;i<leftSize;i++) {
+			leftArray[i]=num[start+i];
+		}
+		
+		for(int j=0;j<rightSize;j++) {
+			rightArray[j]=num[mid+1+j];
+		}
+		int i=0,j=0,k=start;
+		
+		while(i<leftSize && j < rightSize) {
+			if(leftArray[i]>=rightArray[j]) {
+				num[k]=leftArray[i];
+				i++;
+			}else {
+				num[k]=rightArray[j];
+				j++;
+			}
+			k++;
+		}
+		
+		while(i<leftSize) {
+			num[k]=leftArray[i];
+			i++;
+			k++;
+		}
+		
+		while(j<rightSize) {
+			num[k]=rightArray[j];
+			j++;
+			k++;
+		}
+	}
+	public static void mergeSort(int num[],int start, int end) {
+		if(start < end) {
+			int mid = (start+end)/2;
+			mergeSort(num, start, mid);
+			mergeSort(num, mid+1, end);
+			merge(num,start,mid,end);
+		}
+	}
 }
 public class SortingExamples {
 	public static void main(String[] args) {
@@ -84,7 +131,8 @@ public class SortingExamples {
 	}
 	//Sorting.selectionSortAsc(num, num.length);
 	//Sorting.bubbleSortAsc(num, num.length);
-	Sorting.insertionSortAsc(num, num.length);
+	//Sorting.insertionSortAsc(num, num.length);
+	Sorting.mergeSort(num, 0, num.length-1);
 	System.out.println();
 	System.out.println("After sort Asc");
 	for(int i=0;i<num.length;i++) {
