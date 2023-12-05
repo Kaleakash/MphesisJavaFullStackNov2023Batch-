38fe1fd6e542c4e34360ef5a1770ddd6247b9bae
akash <akash300383@gmail.com>
2023-12-05 16:48:59 +0530

    done

--- Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SearchingExample.java
+++ Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SearchingExample.java
@@ -9,16 +9,40 @@ class Searching {
 			}
 			return -1;
 	}
+	
+	public static void binarySearch(int num[], int start,int end, int search) {
+		int mid = (start+end)/2;
+		while(start<=end) {
+			if(num[mid]<search) {
+				start = mid+1;
+			}else if(num[mid]==search) {
+				System.out.println("Element is found "+mid);
+				break;
+			}else {
+				end = mid-1;
+			}
+			mid = (start +end )/2;
+		}
+		if(start > end) {
+			System.out.println("Element is not found");
+		}
+	}
 }
 public class SearchingExample {
 	public static void main(String[] args) {
 		int num[]= {4,1,7,8,6,5,9};
-		int search = 40;
-		int indexPosition = Searching.linearSearch(num, search);
-		if(indexPosition<0) {
-			System.out.println("Element not present");
-		}else {
-			System.out.println("Element present at position "+indexPosition);
+		int search = 7;
+		Sorting.bubbleSortAsc(num, num.length-1);
+		for(int n:num) {
+			System.out.print(n+" ");
 		}
+		System.out.println();
+		Searching.binarySearch(num, 0, num.length-1, search);
+//		int indexPosition = Searching.linearSearch(num, search);
+//		if(indexPosition<0) {
+//			System.out.println("Element not present");
+//		}else {
+//			System.out.println("Element present at position "+indexPosition);
+//		}
 	}
 }
