709aa68bfacb716295ebe80f64fddbeb04aee3f6
akash <akash300383@gmail.com>
2023-12-05 15:25:38 +0530

    done

--- Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SortingExamples.java
+++ Core Java Projects - Course1/SortingAndSearchingTechnique/src/com/SortingExamples.java
@@ -50,23 +50,49 @@ class Sorting{
 			}
 		}
 	}
+	
+	public static void insertionSortAsc(int num[], int size) {
+		for(int i=1;i<size;i++) {
+			int key = num[i];
+			int j=i-1;
+			while(j>=0 && key < num[j]) {
+				num[j+1]=num[j];
+				j--;
+			}
+			num[j+1]=key;
+		}
+	}
+	public static void insertionSortDesc(int num[], int size) {
+		for(int i=1;i<size;i++) {
+			int key = num[i];
+			int j=i-1;
+			while(j>=0 && key > num[j]) {
+				num[j+1]=num[j];
+				j--;
+			}
+			num[j+1]=key;
+		}
+	}
+	
 }
 public class SortingExamples {
 	public static void main(String[] args) {
-	int num[]= {5,1,3,8,6};
+	int num[]= {5,1,3,8,6,2,4,9,7,10};
 	System.out.println("Before sort");
 	for(int i=0;i<num.length;i++) {
 		System.out.print(num[i]+" ");
 	}
 	//Sorting.selectionSortAsc(num, num.length);
-	Sorting.bubbleSortAsc(num, num.length);
+	//Sorting.bubbleSortAsc(num, num.length);
+	Sorting.insertionSortAsc(num, num.length);
 	System.out.println();
 	System.out.println("After sort Asc");
 	for(int i=0;i<num.length;i++) {
 		System.out.print(num[i]+" ");
 	}
 	//Sorting.selectionSortDesc(num, num.length);
-	Sorting.bubbleSortDesc(num, num.length);
+	//Sorting.bubbleSortDesc(num, num.length);
+	Sorting.insertionSortDesc(num, num.length);
 	System.out.println();
 	System.out.println("After sort Desc");
 	for(int i=0;i<num.length;i++) {
