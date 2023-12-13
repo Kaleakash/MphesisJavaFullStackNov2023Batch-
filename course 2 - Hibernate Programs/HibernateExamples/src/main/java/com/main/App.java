beec0f0a30e69811ff11c8ca7d672a26d5aac974
akash <akash300383@gmail.com>
2023-12-13 16:40:34 +0530

    done

--- course 2 - Hibernate Programs/HibernateExamples/src/main/java/com/main/App.java
+++ course 2 - Hibernate Programs/HibernateExamples/src/main/java/com/main/App.java
@@ -5,6 +5,8 @@ import com.utitlity.MenuOption;
 /*
  * 
  * 	create table trainer(tid int primary key,tname varchar(30));
+create table student(sid int primary key,sname varchar(30), age int, tsid int, foreign key(tsid) references trainer(tid));
+
  * 
  * 
  */
