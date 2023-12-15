8a647a0fc34de4953c8d5cf99d3d986793baeda9
akash <akash300383@gmail.com>
2023-12-15 14:34:54 +0530

    done

---
+++ JEE Projects/SimpleServletApp/src/main/java/com/LoginServlet.java
@@ -0,0 +1,48 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		String email = request.getParameter("emailid");
+		String password = request.getParameter("pass");
+		if(email.equals("akash@gmail.com") && password.equals("123")) {
+			pw.println("successfully login");
+		}else {
+			pw.println("failure try once again");
+		}
+		//pw.println("your emaili id is  "+email);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
