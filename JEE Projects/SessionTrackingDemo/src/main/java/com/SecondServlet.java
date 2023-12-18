396f1d9c96bbfea08036d69c939ccb37ccf30024
akash <akash300383@gmail.com>
2023-12-18 17:26:04 +0530

    done

---
+++ JEE Projects/SessionTrackingDemo/src/main/java/com/SecondServlet.java
@@ -0,0 +1,49 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.CookieStore;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter pw = response.getWriter();
+		pw.println("Second Servlet");
+		Cookie cc[] = request.getCookies();
+		for(Cookie c:cc) {
+			pw.println("<br/> "+c.getName()+" "+c.getValue());
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
