4cee548eefa2aa61da86e5578d2dc1eef7508487
akash <akash300383@gmail.com>
2024-01-02 16:49:06 +0530

    done

--- spring boot workspace/SimpleSpringBootProject/src/main/java/com/MyController.java
+++ spring boot workspace/SimpleSpringBootProject/src/main/java/com/MyController.java
@@ -11,4 +11,16 @@ public class MyController {
 		System.out.println("I came here in index");
 		return "index";		// this is your file name without extension. 
 	}
+	
+	@RequestMapping(value = "/hi")		// http://localhost:8080/hi
+	public String hiPage() {
+		System.out.println("I came here in hi path");
+		return "hiPage";		// this is your file name without extension. 
+	}
+	
+	@RequestMapping(value = "/hello")		// http://localhost:8080/hello
+	public String helloPageOpen() {
+		System.out.println("I came here in hello path");
+		return "helloPage";		// this is your file name without extension. 
+	}
 }
