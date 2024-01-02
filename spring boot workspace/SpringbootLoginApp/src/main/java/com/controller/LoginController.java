3829d28db9ac840b78139003f6fdbfb3a7827f18
akash <akash300383@gmail.com>
2024-01-02 17:31:03 +0530

    done

--- spring boot workspace/SpringbootLoginApp/src/main/java/com/controller/LoginController.java
+++ spring boot workspace/SpringbootLoginApp/src/main/java/com/controller/LoginController.java
@@ -1,7 +1,11 @@
 package com.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -11,4 +15,14 @@ public class LoginController {
 		return "login";				// login.jsp open 
 	}
 	
+	@RequestMapping(value = "/checkLogin",method = RequestMethod.POST)
+	public String loginCheckDetails(HttpServletRequest req) {	// DI HttpServletRequest 
+		String emailid = req.getParameter("emailid");
+		String password = req.getParameter("password");
+		if(emailid.equals("akash@gmail.com") && password.equals("akash@123")) {
+			return "success";
+		}else {
+			return "failure";
+		}
+	}
 }
