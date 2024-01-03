1ffffbc0122cb404022f93f47221f55bcd8d15c0
akash <akash300383@gmail.com>
2024-01-03 16:55:28 +0530

    done

--- spring boot workspace/SpringBootWithJPADataPEManagementSystem/src/main/java/com/pems/main/DemoTest.java
+++ spring boot workspace/SpringBootWithJPADataPEManagementSystem/src/main/java/com/pems/main/DemoTest.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // create table project(pid int primary key auto_increment, pname varchar(40));
+// create table employees(id int primary key,name varchar(30), age int, pid int, foreign key(pid) references project(pid));
 @SpringBootApplication(scanBasePackages = "com") // Enable @Controller, @Service, @Component and @Repository
 @EntityScan(basePackages = "com.pems.entity")  		// enable @Entity class. 
 @EnableJpaRepositories(basePackages = "com.pems.repository")// it is use to enable spring jpa data features 
