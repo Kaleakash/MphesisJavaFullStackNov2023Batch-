b7b4537e7142ac70f8b04da9387b5c12172b4f94
akash <akash300383@gmail.com>
2024-01-09 15:46:10 +0530

    done

---
+++ course 3 project/Vaccination-Center-Spring-Boot-Rest-Api/Vaccination-Center-Spring-Boot-Rest-Api/src/main/java/com/vc/entity/Citizens.java
@@ -0,0 +1,85 @@
+package com.vc.entity;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+@Entity
+public class Citizens {
+@Id
+private int cid;
+private String cname;
+private int age;
+private String gender;
+private String phonenumber;
+private String city;
+private String state;
+private String pincode;
+@OneToMany(cascade = CascadeType.ALL)
+@JoinColumn(name = "cid")
+private List<VaccinationCenter> listofvc;
+public int getCid() {
+	return cid;
+}
+public void setCid(int cid) {
+	this.cid = cid;
+}
+public String getCname() {
+	return cname;
+}
+public void setCname(String cname) {
+	this.cname = cname;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getPhonenumber() {
+	return phonenumber;
+}
+public void setPhonenumber(String phonenumber) {
+	this.phonenumber = phonenumber;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getState() {
+	return state;
+}
+public void setState(String state) {
+	this.state = state;
+}
+public String getPincode() {
+	return pincode;
+}
+public void setPincode(String pincode) {
+	this.pincode = pincode;
+}
+public List<VaccinationCenter> getListofvc() {
+	return listofvc;
+}
+public void setListofvc(List<VaccinationCenter> listofvc) {
+	this.listofvc = listofvc;
+}
+@Override
+public String toString() {
+	return "Citizens [cid=" + cid + ", cname=" + cname + ", age=" + age + ", gender=" + gender + ", phonenumber="
+			+ phonenumber + ", city=" + city + ", state=" + state + ", pincode=" + pincode + ", listofvc=" + listofvc
+			+ "]";
+}
+
+}
---
+++ course 3 project/Vaccination-Center-Spring-Boot-Rest-Api/Vaccination-Center-Spring-Boot-Rest-Api/src/main/java/com/vc/entity/VaccinationCenter.java
@@ -0,0 +1,54 @@
+package com.vc.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="vaccinationcenter")
+public class VaccinationCenter {
+@Id
+private int vcid;
+private String hcname;
+private String vcname;
+private LocalDateTime	vdate;
+private Integer cid;
+public int getVcid() {
+	return vcid;
+}
+public void setVcid(int vcid) {
+	this.vcid = vcid;
+}
+public String getHcname() {
+	return hcname;
+}
+public void setHcname(String hcname) {
+	this.hcname = hcname;
+}
+public String getVcname() {
+	return vcname;
+}
+public void setVcname(String vcname) {
+	this.vcname = vcname;
+}
+public LocalDateTime getVdate() {
+	return vdate;
+}
+public void setVdate(LocalDateTime vdate) {
+	this.vdate = vdate;
+}
+public Integer getCid() {
+	return cid;
+}
+public void setCid(Integer cid) {
+	this.cid = cid;
+}
+@Override
+public String toString() {
+	return "VaccinationCenter [vcid=" + vcid + ", hcname=" + hcname + ", vcname=" + vcname + ", vdate=" + vdate
+			+ ", cid=" + cid + "]";
+}
+
+}
