17612a13eb07fff1c1a74e72afe5728f13b69309
akash <akash300383@gmail.com>
2024-01-18 16:34:27 +0530

    done

---
+++ Phase 4 - Frontend technologies/JavaScript using ES6/varLetAndConst.js
@@ -0,0 +1,28 @@
+var a=10;   // declare and initialize int a=10;
+a=20;       // re-initialize a=20;
+var a=30;   // re-declaration int a=30;error in java
+document.write(a);
+
+let b=10;
+b=20;
+//let b=30;     // error becuase b already declared. 
+
+
+// for with var keyword 
+for(var i=0;i<=10000;i++){
+
+}
+document.write("<br/>Value of i is "+i);
+// for with let keyword 
+
+for(let j=0;j<=10000;j++){
+
+}
+//document.write("<br/> Value of j is "+j);
+
+var x=100;
+x=200;
+let y=100;
+y=200;
+const z = 300;
+//z=400;
