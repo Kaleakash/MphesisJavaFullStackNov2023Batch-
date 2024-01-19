741e326d09f7d51ab48952e3f42134ae756a67a3
akash <akash300383@gmail.com>
2024-01-19 14:47:00 +0530

    done

---
+++ Phase 4 - Frontend technologies/JavaScript using ES6/arrayRetrieve.js
@@ -0,0 +1,32 @@
+let num=[100,200,300,400,500];
+// document.write("<br/> "+num);
+// document.write("<br/> "+num.join("->"));
+// document.write("<br/> Retreive element using for loop");
+// for(let i=0;i<num.length;i++){
+//     document.write("<br/> Value is "+num[i]);
+// }
+// document.write("<br/> Retreive element using for in loop")
+// for(let i in num){
+//     document.write("<br/> index "+i+" Value "+num[i]);
+// }
+// document.write("<br/> Retreive element using for of loop")
+// for(let n of num){
+//     document.write("<br/> Value "+n);
+// }
+document.write("<br/> retrieve using callback")
+num.forEach(display);
+
+function display(v,i){
+    document.write("<br/>Value is "+v+" Index is "+i)
+}
+document.write("<br/> retreive using callback as function expression");
+num.forEach(function(v,i){
+    document.write("<br/> VAlue is "+v+" Index is "+i)
+});
+document.write("<br/> retreive using callback as function expression");
+
+num.forEach((v,i)=>document.write("<br/>Value is "+v+"Index is "+i));
+
+
+
+
