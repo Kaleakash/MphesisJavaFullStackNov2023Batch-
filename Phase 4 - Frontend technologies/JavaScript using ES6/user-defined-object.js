ebbd29ce1d0d5950934b24d39b8a17c94db47f34
akash <akash300383@gmail.com>
2024-01-19 15:45:18 +0530

    done

---
+++ Phase 4 - Frontend technologies/JavaScript using ES6/user-defined-object.js
@@ -0,0 +1,6 @@
+// literal style object creation 
+let emp1 = {id:100,name:"Ravi",salary:12000};
+document.write("<br> Object in literal style")
+document.write("<br/> id is "+emp1.id);
+document.write("<br/> name is "+emp1.name);
+document.write("<br/> salary is "+emp1.salary);
