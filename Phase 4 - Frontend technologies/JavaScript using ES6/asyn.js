8b1a4e4c099e304267ce9e2cf02f86897b1a11ce
akash <akash300383@gmail.com>
2024-01-22 14:13:03 +0530

    done

--- Phase 4 - Frontend technologies/JavaScript using ES6/asyn.js
+++ Phase 4 - Frontend technologies/JavaScript using ES6/asyn.js
@@ -16,10 +16,19 @@
 // },1000);
 
 function fun(){
-    setInterval(doTask,1000);
+    setInterval(doTask,1000);   //1st parameter callback and 2nd parameter time 
 }
 var i=0;
 function doTask() {
     document.getElementById("abc").innerHTML=i;
+    if(i%2==0){
+        document.getElementsByTagName("body")[0].style.background="Pink";
+        document.getElementsByTagName("h2")[0].style.color="RED";
+        document.getElementsByTagName("h2")[0].style.fontSize="20pt";
+    }else {
+        document.getElementsByTagName("body")[0].style.background="Yellow";
+        document.getElementsByTagName("h2")[0].style.color="GREEN";
+        document.getElementsByTagName("h2")[0].style.fontSize="40pt";
+    }
     i++;
 }
