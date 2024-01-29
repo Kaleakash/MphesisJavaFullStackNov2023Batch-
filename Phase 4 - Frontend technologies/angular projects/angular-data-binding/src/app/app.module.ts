6caa6a1e683d62c3728fa308df1db8af9e541ca5
akash <akash300383@gmail.com>
2024-01-29 15:50:10 +0530

    done

--- Phase 4 - Frontend technologies/angular projects/angular-data-binding/src/app/app.module.ts
+++ Phase 4 - Frontend technologies/angular projects/angular-data-binding/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 import { ProprtyBindingComponent } from './proprty-binding/proprty-binding.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
 import { TemplateReferenceComponent } from './template-reference/template-reference.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +14,11 @@ import { TemplateReferenceComponent } from './template-reference/template-refere
     StringInterpolationComponent,
     ProprtyBindingComponent,
     EventBindingComponent,
-    TemplateReferenceComponent
+    TemplateReferenceComponent,
+    TwoWayDataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
