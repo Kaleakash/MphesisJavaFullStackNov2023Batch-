f1ea0b6c86da8dce87759813e923d4260f21f848
akash <akash300383@gmail.com>
2024-01-30 17:29:04 +0530

    done

--- Phase 4 - Frontend technologies/angular projects/angular-service/src/app/app.module.ts
+++ Phase 4 - Frontend technologies/angular projects/angular-service/src/app/app.module.ts
@@ -5,15 +5,17 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
 import { MyAngularService } from './my.angularservice';
-
+import { ProductComponent } from './product/product.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpClientModule
   ],
   providers: [MyAngularService],    // provided angular service details. 
   bootstrap: [AppComponent]
---
+++ Phase 4 - Frontend technologies/angular projects/angular-service/src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'    // equal to app.module.ts file in provider section 
+})
+export class ProductService {
+
+  constructor(public http:HttpClient) { }   // pre-defined API di which help to access rest api.
+
+  loadFakeProduct(): void {
+    this.http.get("https://api.escuelajs.co/api/v1/products").subscribe({
+      next:(value:any)=>console.log(value),
+      error:(error:any)=>console.log(error),
+      complete:()=>console.log("done!")
+    });
+    
+  }
+
+}
