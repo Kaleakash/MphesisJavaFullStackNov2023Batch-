ae77ecc9adf061297e4abaaf113e299bef8d614b
akash <akash300383@gmail.com>
2024-01-30 17:51:43 +0530

    done

--- Phase 4 - Frontend technologies/angular projects/angular-service/src/app/product.ts
+++ Phase 4 - Frontend technologies/angular projects/angular-service/src/app/product.ts
@@ -2,6 +2,7 @@ export class Product {          //map to json data.
     constructor(public id:number,
         public title:string,
         public price:number,
-        public description:string
+        public description:string,
+        public images:Array<string>
         ){}
 }
