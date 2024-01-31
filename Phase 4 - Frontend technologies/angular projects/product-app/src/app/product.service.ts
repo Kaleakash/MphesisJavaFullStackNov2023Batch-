a80d6cf60195206501bbec84c74dd1f0f8d49ca1
akash <akash300383@gmail.com>
2024-01-31 15:46:59 +0530

    done

--- Phase 4 - Frontend technologies/angular projects/product-app/src/app/product.service.ts
+++ Phase 4 - Frontend technologies/angular projects/product-app/src/app/product.service.ts
@@ -10,7 +10,18 @@ export class ProductService {
 
   constructor(public http:HttpClient) { }  // DI for HttpClient 
 
+  // get for load the data from rest api 
   loadProductData():Observable<Product[]> {
     return this.http.get<Product[]>("http://localhost:3000/products");
   }
+
+  // post is use store the data
+  // 2 parameter 1st url and 2nd parameter json data ie product data. 
+  storeProduct(product:any): any {
+    return this.http.post("http://localhost:3000/products",product);
+  }
+
 }
+
+
+
--- Phase 4 - Frontend technologies/angular projects/product-app/src/app/product/product.component.ts
+++ Phase 4 - Frontend technologies/angular projects/product-app/src/app/product/product.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from '../product';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit{
+productRef = new FormGroup({
+  id:new FormControl(),
+  name:new FormControl(),
+  price:new FormControl()
+});
+// this function call only once when component loaded...
+ngOnInit(): void {
+    this.loadProducts();
+}
+
 products:Array<Product>=[];
   constructor(public ps:ProductService){}     // DI for Product Service 
   loadProducts() : void {
@@ -16,6 +27,29 @@ products:Array<Product>=[];
       error:(error:any)=>console.log(error),
       complete:()=>console.log("product data loaded...")
     });
-    
+
   }
+msg:string ="";
+  storeProduct(): void {
+    let product = this.productRef.value;
+    console.log(product);
+    let result = this.products.find(p=>p.id==product.id); // if present it return that record 
+                          // else it return undefined. 
+    if(result==undefined){
+      this.ps.storeProduct(product).subscribe({
+        next:(data:any)=>console.log(data),
+        error:(error:any)=>console.log(error),
+        complete:()=>{this.loadProducts()}
+      })
+    }else {
+      this.msg="Product is must be unique";
+    }
+
+    
+    this.productRef.reset();
+  }
+
+
+
+
 }
