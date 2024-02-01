c8974f41eda36c88a17b709244f4e5bd99540217
akash <akash300383@gmail.com>
2024-02-01 14:45:23 +0530

    done

--- Phase 4 - Frontend technologies/angular projects/angular-routing-app/src/app/app.module.ts
+++ Phase 4 - Frontend technologies/angular projects/angular-routing-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
@@ -9,7 +9,6 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +21,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Phase 4 - Frontend technologies/angular projects/angular-routing-app/src/app/login/login.component.ts
+++ Phase 4 - Frontend technologies/angular projects/angular-routing-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  loginRef = new FormGroup({
+    emailid:new FormControl(),
+    password:new FormControl()
+  });
 
 }
