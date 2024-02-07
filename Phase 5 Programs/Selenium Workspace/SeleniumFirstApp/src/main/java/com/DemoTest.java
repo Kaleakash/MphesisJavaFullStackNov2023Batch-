1e6d0b2ac4f56195cf5fe1138fc17d750e399a2f
akash <akash300383@gmail.com>
2024-02-07 15:41:46 +0530

    done

--- Phase 5 Programs/Selenium Workspace/SeleniumFirstApp/src/main/java/com/DemoTest.java
+++ Phase 5 Programs/Selenium Workspace/SeleniumFirstApp/src/main/java/com/DemoTest.java
@@ -8,13 +8,15 @@ public class DemoTest {
 
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "D:\\chrome-win64\\chrome-win64\\chrome.exe");
-		System.out.println("Driver loaded successfully");
-		WebDriver driver = new ChromeDriver();	// created the reference of drive 
-		//WebDriver driver = new EdgeDriver();
+		//System.setProperty("webdriver.chrome.driver", "D:\\chrome-win64\\chrome-win64\\chrome.exe");
+		System.setProperty("webdriver.edge.driver", "D:\\edgedriver_win64\\msedgedriver.exe");
+		//System.out.println("Driver loaded successfully");
+		//WebDriver driver = new ChromeDriver();	// created the reference of drive 
+		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.google.com");  // we are loading google page 
 		String title = driver.getTitle();
 		System.out.println(title);
+		//driver.close();
 	}
 
 }
