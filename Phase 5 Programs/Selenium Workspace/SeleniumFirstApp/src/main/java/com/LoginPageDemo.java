9df92504a025490af0ae35a8ff8b6004fa315ee1
akash <akash300383@gmail.com>
2024-02-07 17:28:20 +0530

    done

--- Phase 5 Programs/Selenium Workspace/SeleniumFirstApp/src/main/java/com/LoginPageDemo.java
+++ Phase 5 Programs/Selenium Workspace/SeleniumFirstApp/src/main/java/com/LoginPageDemo.java
@@ -1,5 +1,8 @@
 package com;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,18 +16,50 @@ public class LoginPageDemo {
 		WebDriver driver = new EdgeDriver();
 		driver.get("http://127.0.0.1:5500/index.html");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		WebElement emailId = driver.findElement(By.id("email"));
-		emailId.sendKeys("akash@gmail.com");
 		WebElement password = driver.findElement(By.id("password"));
-		password.sendKeys("123");
 		WebElement submitButton = driver.findElement(By.id("b1"));
 		WebElement resetButton = driver.findElement(By.id("b2"));
-		String beforeClickUrl = driver.getCurrentUrl();
-		System.out.println(beforeClickUrl);
-		//resetButton.click();
-		submitButton.click();
-		String afterClickUrl = driver.getCurrentUrl();
-		System.out.println(afterClickUrl);
+		
+		
+		
+//		// without writing emailid 
+		// submitButton.click();
+//		Alert alert= driver.switchTo().alert();
+//		System.out.println(alert.getText());
+//		alert.dismiss();
+		
+		
+		// Emailid written but password write
+//		emailId.sendKeys("raj@gmail.com");
+//		submitButton.click();
+//		Alert alert= driver.switchTo().alert();
+//		System.out.println(alert.getText());
+//		alert.dismiss();
+		
+//		// wrong emailid and password 
+//		emailId.sendKeys("raj@gmail.com");
+//		password.sendKeys("123456");
+//		submitButton.click();
+//		Alert alert= driver.switchTo().alert();
+//		System.out.println(alert.getText());
+//		alert.dismiss();
+		
+		// Correct emailid and password
+				emailId.sendKeys("akash@gmail.com");
+				password.sendKeys("123");
+				submitButton.click();
+				Alert alert= driver.switchTo().alert();
+					System.out.println(alert.getText());
+				alert.dismiss();
+				driver.quit();
+//		String beforeClickUrl = driver.getCurrentUrl();
+//		System.out.println(beforeClickUrl);
+//		//resetButton.click();
+//		submitButton.click();
+//		String afterClickUrl = driver.getCurrentUrl();
+//		System.out.println(afterClickUrl);
 	}
 
 }
